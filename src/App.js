7f8fd0671f41bab6d5f9f1e70674c9ba99434bc2
abdugafur <57756396+abdugafur@users.noreply.github.com>
2021-04-06 22:19:55 +0500

    Done Cards components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router className="App">
      <Navbar/>
      <Switch>
-       <Route exact path="/" />
+       <Route exact path="/" component={Home} />
      </Switch>    
     </Router>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cards from '../components/Cards';
+import HeoeSection from '../components/HeoeSection';
+
+function Home() {
+  return (
+    <>
+      <HeoeSection/>
+      <Cards/>
+    </>
+  )
+}
+
+export default Home;
